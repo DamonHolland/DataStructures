e628ee0b9b19edbe4428ed3f4032d9dcbc5f9739
Damon Holland <holl9358@pacificu.edu>
2019-09-19 10:08:34 -0700

    Initial

---
+++ GenericDynamicList/src/driver.c
@@ -0,0 +1,18 @@
+/***************************************************************************
+File name:  driver.c
+Author:     holl9358
+Date:       Sep 19, 2019
+Class:      CS300
+Assignment: 
+Purpose:    
+***************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/list.h"
+
+
+int main(){
+
+	return EXIT_SUCCESS;
+}
---
+++ GenericDynamicList/src/list.c
@@ -0,0 +1,12 @@
+/***************************************************************************
+File name:  list.c
+Author:     Damon Holland
+Date:       Sep 19, 2019
+Class:      CS300
+Assignment: Generic Dynamic List
+Purpose:    
+***************************************************************************/
+
+#include "list.h"
+
+
