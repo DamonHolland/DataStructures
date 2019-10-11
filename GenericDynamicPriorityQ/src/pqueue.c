1f474e7f1385cc5a6567af50c6bec27987930fd8
Damon Holland <holl9358@pacificu.edu>
2019-10-10 22:51:12 -0700

    Initial Setup and Makefile

---
+++ GenericDynamicPriorityQ/src/pqueue.c
@@ -0,0 +1,12 @@
+/***************************************************************************
+File name:  pqueue.c
+Author:     Damon Holland
+Date:       Oct 10, 2019
+Class:      CS300
+Assignment: GenericDynamicPriorityQ
+Purpose:    This file implements the functions for the Priority Queue
+***************************************************************************/
+
+
+#include "stdio.h"
+#include "../include/pqueue.h"
---
+++ GenericDynamicPriorityQ/src/pqueuedriver.c
@@ -0,0 +1,81 @@
+/***************************************************************************
+File name:  pqueuedriver.c
+Author:     Damon Holland
+Date:       Oct 10, 2019
+Class:      CS300
+Assignment: GenericDynamicPriorityQ
+Purpose:    This file is the driver used to test the priority queue
+***************************************************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/pqueue.h"
+
+/**************************************************************************
+ Function: 	 	success
+
+ Description: print a success message
+
+ Parameters:	szStr - the message to print
+ Returned:	 	none
+ *************************************************************************/
+static void success (char * szStr)
+{
+	printf ("SUCCESS: %s\n", szStr);
+}
+
+/**************************************************************************
+ Function: 	 	failure
+
+ Description: print a failure message
+
+ Parameters:	szStr - the message to print
+ Returned:	 	none
+ *************************************************************************/
+static void failure (char * szStr)
+{
+	printf ("FAILURE: %s\n", szStr);
+	exit (EXIT_FAILURE);
+}
+
+/****************************************************************************
+ Function: 	 	assert
+
+ Description: if the expression is true, assert success; otherwise, assert
+ 	 	 	 	 	 	 	failure
+
+ Parameters:	szStr - the message to print
+
+ Returned:	 	none
+ ****************************************************************************/
+static void assert (bool bExpression, char *pTrue, char *pFalse)
+{
+	if (bExpression)
+	{
+		success (pTrue);
+	}
+	else
+	{
+		failure (pFalse);
+	}
+}
+
+/**************************************************************************
+ Function: 	 	main
+
+ Description: test all the functionality of the list
+
+ Parameters:	none
+
+ Returned:	 	Exit Status
+ *************************************************************************/
+int main(){
+
+	PriorityQueue sTheQueue;
+
+
+	assert(true, "Program Builds Successfully!", "Program Build Failed");
+
+	return EXIT_SUCCESS;
+}
