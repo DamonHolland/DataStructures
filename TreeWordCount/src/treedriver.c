f80289f0d78a5b5dfa357fbe088a2dd51b823412
Damon Holland <holl9358@pacificu.edu>
2019-11-04 09:44:16 -0800

    Initial

---
+++ TreeWordCount/src/treedriver.c
@@ -0,0 +1,19 @@
+/***************************************************************************
+File name:  treeDriver.c
+Author:     Damon Holland
+Date:       Nov 4, 2019
+Class:      CS300
+Assignment: 
+Purpose:    
+***************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/tree.h"
+
+
+int main ()
+{
+	printf ("Build Success\n");
+	return EXIT_SUCCESS;
+};
