3a7a4cdd52d43e963079fd3a35e213b52636957f
Damon Holland <holl9358@pacificu.edu>
2019-11-25 21:35:41 -0800

    Invoice Initial Setup and Makefile

---
+++ Invoice/src/invoice.c
@@ -0,0 +1,28 @@
+/**************************************************************************
+File name:  invoice.c
+Author:     Damon Holland
+Date:       Nov 25, 2019
+Class:      CS300
+Assignment: Hash Tables
+Purpose:    
+**************************************************************************/
+#include "stdlib.h"
+#include "stdio.h"
+#include "../../HashTable/include/ht.h"
+#include <stdint.h>
+
+
+/*************************************************************************
+ Function: 	 	main
+
+ Description: Test all the functions of the hash table
+
+ Parameters:	none
+
+ Returned:	 	Exit Status
+ ************************************************************************/
+int main()
+{
+	printf ("Build Success\n");
+	return EXIT_SUCCESS;
+}
